03dddf6c97ebf13e0aaf574bf92116224f159df1
NORA-SH-Widyan <norashawqi7@gmail.com>
2025-12-29 19:04:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,16 +10,12 @@ st.set_page_config(page_title="Math Students Dashboard", layout="wide")
 st.title("📊 Math Students Performance Dashboard")
 st.success("App started successfully ✅")
 
-@st.cache_data
-def load_data():
-    return pd.read_csv(
-        "MathEdataset_dashboard.csv",
-        sep=";",
-        encoding="latin1",
-        on_bad_lines="skip"
-    )
-
-df = load_data()
+df = pd.read_csv(
+    "MathEdataset_dashboard.csv",
+    sep=";",
+    encoding="latin1",
+    on_bad_lines="skip"
+)
 
 st.write("Dataset Preview")
 st.dataframe(df.head())
