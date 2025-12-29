2599fc0497fb95d8e1ccf4339121d017631f92eb
NORA-SH-Widyan <norashawqi7@gmail.com>
2025-12-29 22:17:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -109,3 +109,56 @@ st.divider()
 # Footer
 # =========================
 st.caption("Dashboard created using Streamlit • Math Education Dataset")
+
+
+
+
+
+
+
+#Bar Chart/ Type of Answer
+st.subheader("Distribution of Type of Answer")
+answer_counts = df["Type of Answer"].value_counts().sort_index()
+st.bar_chart(answer_counts)
+
+#Histogram:
+#Student Accuracy (Numeric)
+st.subheader("Student Accuracy Distribution")
+fig, ax = plt.subplots()
+ax.hist(df["student_accuracy"], bins=20)
+ax.set_xlabel("Student Accuracy")
+ax.set_ylabel("Count")
+st.pyplot(fig)
+
+#Question Difficulty
+st.subheader("Question Difficulty Distribution")
+fig, ax = plt.subplots()
+ax.hist(df["question_difficulty"], bins=20)
+ax.set_xlabel("Question Difficulty")
+ax.set_ylabel("Count")
+st.pyplot(fig)
+
+#Question Level (Categorical)
+st.subheader("Question Level Distribution")
+level_counts = df["Question Level"].value_counts()
+st.bar_chart(level_counts)
+
+#Boxplot
+#Final Grade vs Type of Answer
+if "G3" in df.columns:
+    st.subheader("Final Grade by Type of Answer")
+    fig, ax = plt.subplots()
+    df.boxplot(column="G3", by="Type of Answer", ax=ax)
+    ax.set_title("")
+    ax.set_xlabel("Type of Answer")
+    ax.set_ylabel("Final Grade")
+    st.pyplot(fig)
+
+
+st.subheader("Keywords Word Count")
+
+fig, ax = plt.subplots()
+ax.boxplot(df["keywords_word_count"].dropna(), vert=False)
+ax.set_xlabel("Word Count")
+
+st.pyplot(fig)
