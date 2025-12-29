15d4f0ab2e8ba78458084db448b719658713f852
NORA-SH-Widyan <norashawqi7@gmail.com>
2025-12-29 18:40:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-print("hello streamlit")
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+st.set_page_config(page_title="Math Students Dashboard", layout="wide")
+
+st.title("📊 Math Students Performance Dashboard")
+st.success("App started successfully ✅")
+
+# Load data
+df = pd.read_csv(
+    "MathEdataset.csv",
+    sep=";",
+    encoding="latin1",
+    on_bad_lines="skip"
+)
+
+st.write("Dataset Preview")
+st.dataframe(df.head())
